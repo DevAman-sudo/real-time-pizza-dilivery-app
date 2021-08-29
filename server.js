dc597046d1f77680456693673d5ff190cc105925
DevAman-sudo <devaman.net@gmail.com>
2021-08-29 16:53:00 +0545

    created express server

--- server.js
+++ server.js
@@ -2,8 +2,21 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
-
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
 const port = process.env.PORT || 5000;
+
+// file paths
+const viewsPath = path.join(__dirname , '/resources/views');
+
+app.get('/' , (req , res) => {
+    res.render('home');
+});
+
+// set template engine
+app.set('views' , viewsPath);
+app.set('view engine' , 'ejs');
+app.use(expressLayout);
 
 // listening on server 5000
 app.listen(port , () => {
