887aadd39813f48ff5f95a993fa5e33ccb251991
DevAman-sudo <devaman.net@gmail.com>
2021-09-06 09:36:35 +0545

    server development

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ app.get('/cart' , (req , res) => {
     res.render('customers/cart');
 });
 
+app.get('/login' , (req , res) => {
+    res.render('auth/login');
+});
+
+app.get('/register' , (req , res) => {
+    res.render('auth/register');
+});
+
 // listening on server 5000
 app.listen(port , () => {
     console.log(`app is listening on port ${port}`);
