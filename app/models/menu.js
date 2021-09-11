2899245e42f256f6926f22f33dcd8947f782ac8e
DevAman-sudo <devaman.net@gmail.com>
2021-09-11 15:13:34 +0545

    database connection sucess

---
+++ app/models/menu.js
@@ -0,0 +1,14 @@
+// npm packages
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const menuSchema = new Schema({
+    name: { type: String, required: true },
+    prize: { type: Number, required: true },
+    image: { type: String, required: true },
+    size: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Menu' , menuSchema);
+
--- server.js
+++ server.js
@@ -4,11 +4,26 @@ const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 // file paths
 const viewsPath = path.join(__dirname , '/resources/views');
 const staticPath = path.join(__dirname , './public');
+
+// mongodb database connection
+const databaseUrl = "mongodb://localhost/pizza";
+mongoose.connect( databaseUrl ,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: true,
+        // useCreateIndex: true
+    }).then(() => {
+        console.log('database connected');
+    }).catch((error) => {
+        console.log(`error occured while database connection => ${error}`);
+    });
 
 // assets
 app.use(express.static(staticPath));
