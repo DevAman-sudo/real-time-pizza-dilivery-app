583e0fea13f49dfd7decfb971e9adbffe7b29fac
DevAman-sudo <devaman.net@gmail.com>
2021-09-11 15:51:40 +0545

    displayed fetched data from database

--- app/http/controllers/homeControllers.js
+++ app/http/controllers/homeControllers.js
@@ -1,8 +1,11 @@
+const Menu = require('../../models/menu');
+
 function homeControllers() {
 
     return {
-        index(req , res) {
-            res.render('home');
+        async index(req , res) {
+            const pizzas = await Menu.find();
+            res.render('home' , {pizzas: pizzas});
         }
     }
 
