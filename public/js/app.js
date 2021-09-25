e82bf7d3faf2f3226bf54a79b6552098cc5633f0
DevAman-sudo <devaman.net@gmail.com>
2021-09-25 14:30:31 +0545

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -27875,8 +27875,8 @@ function _initStripe() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/codersgyan/Documents/youtube/realtime-pizza/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/codersgyan/Documents/youtube/realtime-pizza/resources/scss/app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! D:\web_projects\real-time-pizza-dilivery-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\web_projects\real-time-pizza-dilivery-app\resources\scss\app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
